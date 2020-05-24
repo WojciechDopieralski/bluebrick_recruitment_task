ec6b6f27073f91ee1fa5554c8dcb184080691b90
WojciechDopieralski <wojciech.dopieralski@op.pl>
2020-05-24 20:30:49 +0200

    Report progress.
    additional nice pie charts.
    
    New data about avarage price.
    
    Created R failes for saving function and data preparation tables.

--- data_preparation.R
+++ data_preparation.R
@@ -31,6 +31,9 @@ game_is_promo <- data_column_choice %>%
   ) %>%
   arrange(country_code)
 
+sales_period <- game_is_promo %>%
+  filter(winter_sales != "no_ws", is_promo == T)
+
 date_group <- game_is_promo %>%
   group_by(date) %>%
   summarise(numb = n(), sum_revenu = sum(net_sales_usd), sum_net_sales = sum(net_units_sold), 
@@ -48,63 +51,22 @@ date_group <- game_is_promo %>%
                                                 "ws2",
                                                 "no_ws"))))
 
+only_ws_data <- date_group %>%
+  filter(winter_sales != "no_ws") %>%
+  group_by(winter_sales) %>%
+  mutate(rev_promo_rate = round(sum_promo_rev/lag(sum_promo_rev) * 100,2)) %>%
+  ungroup()
 
+mean_price <- only_ws_data %>%
+  group_by(winter_sales) %>%
+  summarise(mean_unit = round(mean(unit_net_rev),2)) %>%
+  ungroup() %>%
+  mutate(discount = c(0.32,0.4),
+         norm_price = round((mean_unit/(1-discount)),2))
 
-count_occurencies <- function(data, ...) {
-  result <-data %>%
-    group_by(...) %>%
-    summarise(numb = n()) %>%
-    mutate(perc_group = numb/sum(numb)) %>%
-    ungroup() %>%
-    mutate(perc_ungroup = numb/sum(numb))
-  return(result)
-}
+data_pie_chart_s1 <- date_group %>%
+  filter(sales_season == 1)
 
-count_param <- function(data, col_to_sum, ...) {
-  col_name <- enquo(col_to_sum)
-  result <- data %>%
-    group_by(...) %>%
-    summarise(numb = n(), sum_net = sum(!!col_name)) %>%
-    mutate(count_sum_group = sum_net/sum(sum_net)) %>%
-    ungroup() %>%
-    mutate(count_sum_ungroup = sum_net/sum(sum_net))
-  return(result)
-}
+data_pie_chart_s2 <- date_group %>%
+  filter(sales_season == 2)
 
-create_waffle_chart <- function(data, fill, values, subtitle) {
-  ggplot(data, aes(fill = {{fill}}, values = {{values}})) +
-    geom_waffle(n_rows = 10, size = 0.33, colour = "white", flip = TRUE) +
-    labs(subtitle=subtitle) +
-    coord_equal() +
-    theme_enhance_waffle() +
-    waffle_theme_custom
-}
-
-create_waffle_count <- function(data, fun_name, ...) {
-  fun_name(data, ...) %>%
-    mutate(perc_chart = percent(perc_group),
-           waffle_chart = round(perc_group*100))
-}
-
-create_waffle_param <- function(data, fun_name, ...) {
-  fun_name(data, ...) %>%
-    mutate(perc_chart = percent(count_sum_group),
-           waffle_chart = round(count_sum_group*100))
-}
-
-basic_line_season <- function (y_val, title ="add title", ylab_title = "name_lab", df_data = date_group) {
-  ggplot_obj <- ggplot(data = df_data, aes(x = day_month, y = {{y_val}}, group = sales_season, colour = sales_season)) +
-    geom_line() +
-    geom_point() +
-    labs(title=title,
-         x = "Day and Month of sales",
-         y = ylab_title) +
-    simple_line_theme
-  
-  plotly_ojb <- ggplotly(ggplot_obj) 
-  
-  ret_obj <- plotly_ojb %>% 
-    layout(legend = list(title=list(text='<b>Sales Season:</b>')))
-  
-  return(ret_obj)
-}
---
+++ theme.R
@@ -0,0 +1,32 @@
+waffle_theme_custom <- theme(
+  plot.title = element_text(hjust = 0.5, size = 12, face = "bold"),
+  plot.subtitle = element_text(hjust = 0.5, size = 10, face = "italic"),
+  panel.background = element_rect(fill = "transparent", colour = NA),
+  legend.text = element_text(hjust = 0.5, size = 9),
+  legend.title = element_blank(),
+  legend.key = element_rect(size = 5),
+  legend.position = "bottom",
+  legend.justification = "center",
+  legend.margin = margin(0, 0, 0, 0),
+  legend.spacing.x = unit(0, "pt"),
+  plot.caption = element_text(hjust = 1, face = "italic"),
+  axis.ticks = element_blank(),
+  axis.title = element_blank(),
+  axis.text = element_blank(),
+  plot.margin=unit(c(1,1,1,1), "lines", )
+)
+
+simple_line_theme <- theme(
+  plot.title = element_text(hjust = 0.5, size = 12, face = "bold"),
+  plot.subtitle = element_text(hjust = 0.5, size = 10, face = "italic"),
+  axis.text.x = element_text(angle = 90, hjust = 1, size = 8, face = "bold"),
+  axis.text.y = element_text(face ="italic"),
+  legend.text = element_text(hjust = 0.5, size = 9),
+  legend.position = c(0.8, 0.2),
+  legend.title = element_blank(),
+  legend.justification = "center",
+  axis.title = element_text(hjust = 0.5, size = 10, face = "bold"),
+  panel.background = element_rect(fill = "transparent", colour = NA),
+  plot.margin=unit(c(1,1,1,1), "lines"),
+  plot.caption = element_text(hjust = 1, face = "italic"),
+)
